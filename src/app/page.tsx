6dba773de9534f308fdeaa3cdf6ad39ae1fd8998
shanecranor <shanecranor@gmail.com>
2024-08-09 21:07:01 -0500

    prettier :p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import "./page.scss"
+import "./page.scss";
 import { Clock } from "@/components/clock";
 
 export default function Home() {
   return (
-   <div>
-    hello world
-    <Clock/>
-   </div>
+    <div>
+      <footer>
+        <Clock />
+      </footer>
+    </div>
   );
 }

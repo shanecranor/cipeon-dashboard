5b254025adf9fb439b865a3cbea015e1ca65c01f
Justin <peterjust.d@gmail.com>
2024-08-09 22:17:44 -0500

    first change

--- src/components/VersionBanner.tsx
+++ src/components/VersionBanner.tsx
@@ -3,6 +3,7 @@ export const VersionBanner = () => {
     <div className="p-version-banner">
       <p>Hack Night</p>
       <p>v0.{weeksPast(1723256301120 - 4320000) + 8}</p>
+      <p>from infinity to beyond</p>
     </div>
   );
 };

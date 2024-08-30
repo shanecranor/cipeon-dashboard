c109455886736e9c24352ad22ec3d211e2118be2
Justin <peterjust.d@gmail.com>
2024-08-29 21:04:45 -0500

    FIXED DATE CALCULATIONS

--- src/components/VersionBanner.tsx
+++ src/components/VersionBanner.tsx
@@ -1,17 +1,21 @@
+const lastMajorVersion = new Date('2024-8-22') // 1.0 - Friday
+const indexDate = new Date(); 
+indexDate.setDate(lastMajorVersion.getDate() - 1);
+
 export const VersionBanner = () => {
   return (
     <div className="p-version-banner">
       <p>Hack Night</p>
-      <p className="pulsing">v1.{weeksPast(new Date('2024-8-26').getTime()) + 1}</p>
+      <p className="pulsing">v1.{weeksPast(indexDate)}</p>
       <p className="tagline">action reaction</p>
     </div>
   );
 };
 
-function weeksPast(start: number) {
+function weeksPast(start: Date) {
   const currentDate = Date.now();
   const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-  const elapsedMilliseconds = currentDate - start;
+  const elapsedMilliseconds = currentDate - start.getTime();
   const weeks = Math.floor(elapsedMilliseconds / millisecondsPerWeek);
   return weeks;
 }

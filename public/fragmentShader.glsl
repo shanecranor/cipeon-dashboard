520ca2793e927de949cc16fec52d1588f7ec56e4
shanecranor <shanecranor@gmail.com>
2024-09-13 21:32:57 -0500

    funky waves background sppoooky

--- public/fragmentShader.glsl
+++ public/fragmentShader.glsl
@@ -5,6 +5,19 @@ float pixelSize = 0.5;  // size of the pixels for pixelation effect
 float distanceToPoint(vec2 p1, vec2 p2) {
     return sqrt(pow(p1.x - p2.x, 2.0) + pow(p1.y - p2.y, 2.0));
 }
+
+vec3 yuv2rgb(vec3 yuv) {
+    float y = yuv.x;
+    float u = yuv.y - 0.5;
+    float v = yuv.z - 0.5;
+
+    float r = y + 1.402 * v;
+    float g = y - 0.344136 * u - 0.714136 * v;
+    float b = y + 1.772 * u;
+
+    return vec3(r, g, b);
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // Retrieve screen resolution and pixel coordinates
@@ -14,7 +27,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv = floor(uv * pixelSize) / pixelSize;
 
     // Time value limited to 20 FPS (1/20th of a second)
-    float frameRate = 60.0;
+    float frameRate = 30.0;
     float frameTime = 1.0 / frameRate;
     float tempTime = floor(iTime / frameTime) * frameTime * -0.1;
 
@@ -27,8 +40,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     //     mod(abs((mod(x / 10.0, 100.0) * y * tan(x * 10.0 + tempTime) * 10.0 )+ 400000.0)  , 16777215.0)
     // );
     float colorHex = floor(
-        mod(abs(
-          (distanceToPoint(vec2(sin(x/500.0)*1000.0, 20.0+(sin(y)*3000.0)), vec2(x/10.0, y/10.0)) + tempTime*5000.0) * 11.0 +((y*100.0+x*10.0)/5.0)
+        mod(0.3*abs(
+          (distanceToPoint(vec2(x*x*x/100.0+tempTime*10.0, (y*y*100.0-tempTime*20000.0)+cos(x/100.0+tempTime*10.0)*10000000.0), vec2(10000.0+sin(x)*1000000.0,0.0))*1.0 - tempTime*5000.0) 
           //(mod(x / 10.0, 100.0) * y * tan(x * 10.0 + tempTime) * 10.0 ) + 400000.0
           ), 16777215.0)
     );
@@ -39,7 +52,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float b = floor(mod(colorHex, 256.0)) / 255.0;
 
     // Combine into a color vector
-    vec3 color = vec3(r*60.0+g/2.0, g/4.0, b*b*b*100.0);
+    // vec3 color = vec3(r*30.0+g/2.0, g/2.0, b*b*b*100.0);
+    vec3 color = yuv2rgb(vec3(r, g, b));
 
     // Set the final fragment color
     fragColor = vec4(color, 1.0);

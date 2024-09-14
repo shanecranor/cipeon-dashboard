a99f1dbd828d1ff3aaed3368dc898b81e8b4d1a3
shanecranor <shanecranor@gmail.com>
2024-09-13 20:28:53 -0500

    fix hydration issue

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -3,7 +3,7 @@ import { useLightningTimeClock } from "@purduehackers/time/react";
 export const Clock = () => {
   const { lightningString, formattedNormalTime } = useLightningTimeClock(); // Surfaces the entire Lightning Time object and an additional formattedNormalTime
   return (
-    <h1>
+    <h1 suppressHydrationWarning>
       {lightningString} ({formattedNormalTime})
     </h1>
   );
--- src/components/VersionBanner.tsx
+++ src/components/VersionBanner.tsx
@@ -1,16 +1,18 @@
-"use client"
+"use client";
 const lastMajorVersion = new Date("2024-8-22"); // 1.0 - Friday
 const indexDate = lastMajorVersion;
 indexDate.setDate(lastMajorVersion.getDate() - 1);
 
 export const VersionBanner = () => {
   return (
-    <div className="p-version-banner">
+    <div className="p-version-banner" suppressHydrationWarning>
       <p>Hack Night</p>
       <p className="pulsing">v1.{weeksPast(indexDate)}</p>
       <p className="tagline">it begin</p>
       <br></br>
-      <p className="info"> build instant: {Date.now()} </p>
+      <p className="info" suppressHydrationWarning>
+        build instant: {Date.now()}{" "}
+      </p>
     </div>
   );
 };
